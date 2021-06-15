e6883d7e4799e9e87e984575ec22f57b870fdd43
Christopher Swinton <33087553+Swinny96@users.noreply.github.com>
2021-06-15 12:50:53 +0100

    Updated packages

--- gatsby-config.js
+++ gatsby-config.js
@@ -23,7 +23,7 @@ module.exports = {
         background_color: `#663399`,
         theme_color: `#663399`,
         display: `minimal-ui`,
-        // icon: `${__dirname}/static/images/favicon/favicon-16x16.png`, // This path is relative to the root of the site.
+        icon: `${__dirname}/static/images/favicon/favicon-16x16.png`, // This path is relative to the root of the site.
       },
     },
     // this (optional) plugin enables Progressive Web App + Offline functionality
